fc534d606067a2c4fdaf4f20b12b6384f23cef59
King_DuckZ <king_duckz@gmx.com>
2012-11-01 18:29:10 +0000

    Adding more features. One such feature is the ability to loop over search results. WiP on the sort method.

--- src/searchwrapper.rb
+++ src/searchwrapper.rb
@@ -1,6 +1,13 @@
 require 'ebservice.rb'
 
 class SearchWrapper < Ebay::EbService
+	#See: http://developer.ebay.com/DevZone/finding/CallRef/findItemsByKeywords.html#Request.sortOrder
+	@@SortModes = %w{
+		BestMatch BidCountFewest BidCountMost CountryAscending CountryDescending
+		CurrentPriceHighest DistanceNearest EndTimeSoonest
+		PricePlusShippingHighest PricePlusShippingLowest StartTimeNewest
+	}
+
 	attr_reader :appID
 	attr_accessor :localSearchOnly
 
@@ -9,7 +16,12 @@ class SearchWrapper < Ebay::EbService
 		@appID = parAppID
 		@postalCode = nil
 		@localSearchOnly = false
+		@sortMode = 0
 		nil
+	end
+
+	def sortMode()
+		return @@SortModes[@sortMode]
 	end
 
 	def postalCode()
@@ -21,27 +33,41 @@ class SearchWrapper < Ebay::EbService
 		@postalCode = parNew
 	end
 
-	def findByKeywords(parKeywords)
+	def each_result(parKeywords)
 		raise ArgumentError, "Invalid parameter type: \"#{parKeywords.class.name}\"" unless parKeywords.is_a?(String)
 		raise ArgumentError, "Unclean keywords" if parKeywords.tainted?
 
-		params = { "keywords" => parKeywords }
-		unless @postalCode.nil? then
-			params["buyerPostalCode"] = @postalCode
-			params["sortOrder"] = "Distance"
+		retArray = nil
+		pageNum = 1
+		z = 0
+		while (retArray = self.getPageResult(parKeywords, pageNum)).size > 0 do
+			retArray.each do |itm|
+				itm["internalItemNum"] = z
+				itm["internalPageNum"] = pageNum
+				yield(itm)
+				z += 1
+			end
+			pageNum += 1
 		end
+		raise StopIteration
+	end
+
+	def getPageResult(parKeywords, parPage)
+		raise ArgumentError, "Invalid parameter type: \"#{parKeywords.class.name}\"" unless parKeywords.is_a?(String)
+		raise ArgumentError, "Unclean keywords" if parKeywords.tainted?
+		return Array.new if parPage > 100 #Apparently the api has a hard limit
+
+		params = { "keywords" => parKeywords }
+		params["paginationInput"] = {"pageNumber" => parPage.to_s, "entriesPerPage" => "100"}
+		params["sortOrder"] = @@SortModes[@sortMode]
+		params["buyerPostalCode"] = @postalCode unless @postalCode.nil?
 		if @localSearchOnly then
 			params.fetch("itemFilter", Array.new) << { "name" => "LocalSearchOnly", "value" => "true" }
 		end
 
 		hResult = self.callService(:FindingService, "findItemsByKeywords", "1.11.0", params, @appID)
 
-		#ack
-		#version
-		#timestamp
-		#searchResult
-		#paginationOutput
-		#itemSearchURL
+		#ack, version, timestamp, searchResult, paginationOutput, itemSearchURL
 		if !hResult["ack"].is_a?(Array) || hResult["ack"].first != "Success" then
 			raise StandardError, "Search failed with error: #{hResult["ack"]}"
 		end
